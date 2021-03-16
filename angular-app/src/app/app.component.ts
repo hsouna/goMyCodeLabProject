959896f1e465a3059edb6d05c0b3e0888cfe3186
hassen mezghani <mezghanni.hassen@gmail.com>
2021-03-16 13:46:18 +0100

    update apiURL

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   // Link to our api, pointing to localhost
-  API = 'http://express-network:3000';
+  API = 'http://localhost:3000';
 
   // Declare empty list of people
   people: any[] = [];
